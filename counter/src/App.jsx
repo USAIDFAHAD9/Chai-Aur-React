7575812542a57dda6c6e46be9273d4ed57636d78
Usaid Fahad <usaidfahad9@gmail.com>
2024-03-01 23:36:43 +0530

    made new counter app

---
+++ counter/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addValue = () => {
+    setCount(count + 1)
+  }
+  const decreaseValue = () => {
+    if (count > 0) setCount(count - 1)
+  }
+  return (
+    <>
+      <h1>chai aur react counter</h1>
+      <h2>Counter Value : {count}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={decreaseValue}>Decrease Value</button>
+    </>
+  )
+}
+
+export default App
